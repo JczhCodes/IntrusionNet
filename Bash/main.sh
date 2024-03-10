f837615b33add9247653dec2907f138fd6a48b57
Jonathan Chiu <101392243+JczhStudy@users.noreply.github.com>
2024-03-11 02:35:12 +0800

    Bash

---
+++ Bash/main.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source "./openAI.sh"
+
+check_key
+
+asst_id=$(create_assistant)
+
+thread_id=$(create_thread)
+
+# retrieve_thread $thread_id
+
+initial_question="The IP of the client's machine is 192.168.18.129. Begin!"
+create_message "$initial_question"
+
+send_message
+
+sleep 60
+
+retrieve_thread
